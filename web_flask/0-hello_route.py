99b8a32f6971ff6a15db2aaf166eef01730ad55a
Tentswalo <104768179+Tentswalo@users.noreply.github.com>
2023-01-15 22:15:11 +0200

    clone

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,14 +3,17 @@
 '''
 from flask import Flask
 
+
 app = Flask(__name__)
 '''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def index():
-        '''The home page.'''
-            return 'Hello HBNB!'
+    '''The home page.'''
+    return 'Hello HBNB!'
 
-            if __name__ == '__main__':
-                    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
